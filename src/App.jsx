7979f0dfcdcf077a15d01c9fd57c1129357efcba
Aditya Kumar <124980341+adixoo@users.noreply.github.com>
2024-10-08 15:13:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
 import "./App.css";
 import { signal } from "@preact/signals-react";
 
+const numbers = signal([1, 2, 3, 4, 5]);
 const count = signal(0);
 function App() {
    return (
       <>
-         <button onClick={() => count.value++}>count is {count.value}</button>
+         <button
+            onClick={() => {
+               numbers.value = [...numbers.value, numbers.value.length + 1];
+               count.value = count.value + 1;
+            }}
+         >
+            count:{count} <br />
+            count.value:{count.value}
+         </button>
+
+         {numbers.value.map((i) => (
+            <div key={i}>{i}</div>
+         ))}
       </>
    );
 }
